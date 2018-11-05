924c793465e4a6f4a55e1c11f094fcd74cbfff61
Neil Froggatt <neilfr@live.com>
2018-11-05 10:57:25 -0500

    now displaying giphys and ratings

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -23,10 +23,34 @@ $("#addTopic").on("click", function(event) {
 
 // display giphys
 function displayGiphys(){
+    $('#resultsContainer').empty(); //clears the previous giphy list
+
     var $element=$(this);
     var topic=$element.attr('btnTopic');
     console.log("topic of button clicked is:");
     console.log(topic);
+
+    //build the query, submit the get request, and capture the response
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q="+topic+"&api_key=Y44NDKEHx6dsp5smn0MUNkIU6Ml60CPk&limit=10";
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function(response){
+        console.log(response);
+        var resultData=response.data;
+
+        //append rating and image for each of the returned items to the screen
+        for (var i=0;i<resultData.length;i++){
+            var $thisGiphyDiv=$('<div>'); //container for the elements for this object
+            var $p=$('<p>');
+            var $img=$('<img>');
+            $img.attr('src',resultData[i].images.fixed_height.url);
+            $p.html(resultData[i].rating);
+            $thisGiphyDiv.append($p);
+            $thisGiphyDiv.append($img);
+            $('#resultsContainer').append($thisGiphyDiv);
+        }
+    })
 }
 
 // display the giphys when the button is clicked
